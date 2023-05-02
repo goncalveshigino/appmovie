2510a6c1cc23579bd5b0798dcb030c0389ef9250
goncalveshigino <goncalveshigino20@gmail.com>
2023-05-02 14:11:53 +0100

    add swiper

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:appcinema/presentation/providers/movies/movies_provider.dart';
+import 'package:appcinema/presentation/widgets/movies/movies_slideshow.dart';
+import 'package:appcinema/presentation/widgets/shared/custom_appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -35,14 +37,14 @@ class _HomeViewState extends ConsumerState<_HomeView> {
     final nowPlayingMovies = ref.watch( nowPlayingMovieProvider );
 
 
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder:(context, index){
-        final movie = nowPlayingMovies[index];
-        return ListTile(
-          title: Text(movie.title),
-        );
-      } ,
+    return Column(
+      children: [
+
+       const CustomAppBar(),
+
+        MoviesSlideShow(movies: nowPlayingMovies)
+      ],
+
     );
   }
 }
---
+++ lib/presentation/widgets/widgets.dart
@@ -0,0 +1,4 @@
+
+
+export 'shared/custom_appbar.dart';
+export 'movies/movies_slideshow.dart';
